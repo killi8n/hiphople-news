eed3f672f5b4d22a09722090d4c8b0a5532ae57c
killi8n <killi8n.dev@gmail.com>
2020-10-04 17:22:54 +0900

    fix: fixed build options

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,33 @@
-import typescript from '@rollup/plugin-typescript'
-import { getBabelOutputPlugin } from '@rollup/plugin-babel'
+import path from 'path'
+import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
 import pkg from './package.json'
 
 export default [
     {
         input: './src/index.ts',
         output: {
-            dir: pkg.main,
-            format: 'cjs',
+            dir: 'dist',
+            format: 'es',
+            preserveModules: true,
             plugins: getBabelOutputPlugin({
                 presets: ['@babel/preset-env'],
             }),
         },
-        plugins: [typescript()],
+        external: [
+            ...Object.keys(pkg.dependencies),
+            ...Object.keys(pkg.devDependencies),
+            /@babel\/runtime/,
+        ],
+        plugins: [
+            resolve({ extensions: ['.ts', '.js'] }),
+            babel({
+                configFile: path.resolve(__dirname, '.babelrc'),
+                extensions: ['.ts', '.js'],
+                babelHelpers: 'runtime',
+                skipPreflightCheck: true,
+                include: ['src/**/*'],
+            }),
+        ],
     },
 ]
